939ab83504f4eac479e97bc45ab3adefbaffa62a
christianastudillo <chris.astu134@gmail.com>
2026-06-04 02:20:05 -0500

    creacion footer

---
+++ src/app/shared/components/footer/footer.component.ts
@@ -0,0 +1,12 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-footer',
+  imports: [RouterLink],
+  templateUrl: './footer.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class FooterComponent {
+  protected readonly year = 2026;
+}
